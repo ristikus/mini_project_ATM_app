30dc8c48c79f13db8abdc4ab3e5e23372f42b35b
Ristia Nadia Permata <ristikus@gmail.com>
2020-12-22 18:31:23 +0700

    Latihan 11

--- atm_program.py
+++ atm_program.py
@@ -6,13 +6,21 @@ from customer import Customer
 customer = Customer(random.randint(0,99999))
 
 while True:
-    trial = 0
     id = int(input("Masukkan pin Anda: "))
+    trial = 1
 
-    while customer.checkPin(id) and trial < 3:
+    while not customer.check_pin(id) and trial < 3:
         id = int(input("Pin anda salah. Silakan masukkan lagi: "))
         trial += 1
 
         if trial == 3:
             print("Error. Silakan ambil kartu dan coba lagi...")
             exit()
+
+    while True:
+        print("Selamat datang di ATM")
+        print("1. Cek Saldo")
+        print("2. Debit")
+        print("3. Simpan")
+        print("4. Ganti pin")
+        print("0. Keluar")
