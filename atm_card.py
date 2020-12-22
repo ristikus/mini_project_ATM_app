28870030b21d8d0964f418df3533e6869ac6f7d1
Ristia Nadia Permata <ristikus@gmail.com>
2020-12-22 16:23:53 +0700

    Latihan 3

--- atm_card.py
+++ atm_card.py
@@ -2,3 +2,19 @@ class ATMCard:
     def __init__(self, default_pin, default_balance):
         self.__default_pin = default_pin
         self.__default_balance = default_balance
+
+    @property
+    def default_pin(self):
+        pass
+
+    @default_pin.getter
+    def default_pin(self):
+        return self.__default_pin
+
+    @property
+    def default_balance(self):
+        pass
+
+    @default_balance.getter
+    def default_balance(self):
+        return self.__default__balance
