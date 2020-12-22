04e8f563c52e73a5dae9337bb21f1005c3ec0314
Ristia Nadia Permata <ristikus@gmail.com>
2020-12-22 16:39:21 +0700

    Latihan 5

--- customer.py
+++ customer.py
@@ -1,6 +1,31 @@
 from atm_card import ATMCard
 
 class Customer:
-    def __init__(self, cust_pin = 1234, cust_balance = 10000):
+    def __init__(self, id, cust_pin = 1234, cust_balance = 10000):
+        self.__id = id
         self.__cust_pin = 1234
         self.__cust_balance = 10000
+
+    @property
+    def id(self):
+        pass
+
+    @id.getter
+    def id(self):
+        return self.__id
+
+    @property
+    def cust_pin(self):
+        pass
+
+    @cust_pin.getter
+    def cust_pin(self):
+        return self.__cust_pin
+
+    @property
+    def cust_balance(self):
+        pass
+
+    @cust_balance.getter
+    def cust_balance(self):
+        return self.__cust_balance
