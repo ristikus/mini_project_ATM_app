e1283b0a3aa3c4bdd281c488dd448dd0bef998b3
Ristia Nadia Permata <ristikus@gmail.com>
2020-12-22 17:38:12 +0700

    Latihan 9

--- atm_program.py
+++ atm_program.py
@@ -4,11 +4,14 @@ import datetime
 from customer import Customer
 
 customer = Customer(random.randint(0,99999))
-condition = True
 
-while condition:
-    limit = 1
+while True:
+    limit = 0
     id = int(input("Masukkan pin Anda: "))
 
-    while customer.checkPin(id):
-        pass  
+    while customer.checkPin(id) and limit < 3:
+        id = int(input("Pin anda salah. Silakan masukkan lagi: "))
+        limit += 1
+
+        
+
